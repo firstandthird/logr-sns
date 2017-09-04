8b2f1cbe4e3dcc5ddeec03dd5f719ab0645c3204
orthagonal <orthagonal1@gmail.com>
2017-09-04 18:27:15 -0500

    over-ride AWS credentials with options

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 'use strict';
 const AWS = require('aws-sdk');
-const sns = new AWS.SNS();
 
 exports.defaults = {
   clientId: process.env.AWS_ACCESS_KEY_ID,
   secretId: process.env.AWS_SECRET_ACCESS_KEY,
   phoneNumbers: [] // 11-digit phone number starting with 1
 };
+
+let sns = false;
+
 exports.log = function(options, tags, message) {
+  if (!sns) {
+    AWS.config.update({
+      accessKeyId: options.clientId,
+      secretAccessKey: options.secretId
+    });
+    sns = new AWS.SNS();
+  }
   options.phoneNumbers.forEach((number) => {
     const params = {
       Message: `[${tags}] ${message}`,
